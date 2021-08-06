92979f0ae66a1ac364b0385e8fd88868cb255a50
David Rogers <david.rogers@monterail.com>
2021-08-06 11:46:59 +0200

    edited migration

--- db/migrate/20210805112956_create_seats.rb
+++ db/migrate/20210805112956_create_seats.rb
@@ -3,9 +3,9 @@
 class CreateSeats < ActiveRecord::Migration[6.1]
   def change
     create_table :seats do |t|
-      t.string :seat_number
-      t.integer :cinema_number
-      t.string :seat_price
+      t.string :seat_number, null: false
+      t.integer :cinema_number, null: false
+      t.string :seat_price, default: 0
 
       t.timestamps
     end

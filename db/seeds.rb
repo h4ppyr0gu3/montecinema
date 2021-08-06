118daa80c5899f78fe6308c3a1a95330bb410c33
David Rogers <david.rogers@monterail.com>
2021-08-06 12:21:14 +0200

    fixed seeds and spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 def seed_cinema(rows, columns, cinema_number)
-  columns.times do |i|
-    rows.times do |j|
-      seat_letter_order = 'a'.ord + i
-      seat_letter = seat_letter_order.chr
-      seat_number = "#{seat_letter}-#{j + 1}"
-      seat = Seat.new(cinema_number: cinema_number, seat_number: seat_number)
-      if seat.save
-        Rails.logger.info "cinema_number: #{cinema_number}, seat_number: #{seat_number}"
-      elsif seat.errors.any?
-        Rails.logger.error seat.errors.messages
-      end
+  cols = ("a".."z").take(columns).to_a
+  rows = (1..rows).to_a
+  seats = cols.product(rows).map(&:join)
+  seats.each do |seat_number|
+    seat = Seat.new(cinema_number: cinema_number, seat_number: seat_number)
+    if seat.save
+      Rails.logger.info "cinema_number: #{cinema_number}, seat_number: #{seat_number}"
+    elsif seat.errors.any?
+      Rails.logger.error seat.errors.messages
     end
   end
 end
--- spec/models/seat_spec.rb
+++ spec/models/seat_spec.rb
@@ -5,15 +5,15 @@ require 'rails_helper'
 RSpec.describe Seat, type: :model do
   it 'ensure stopping incorrect validations' do
     Seat.create!(cinema_number: 1, seat_number: 'a-1')
-    exception = Seat.new(cinema_number: 1, seat_number: 'a-1')
-    exception.validate
-    expect(exception.errors[:seat_number]).to include('has already been taken')
+    invalid_seat = Seat.new(cinema_number: 1, seat_number: 'a-1')
+    invalid_seat.validate
+    expect(invalid_seat.errors[:seat_number]).to include('has already been taken')
   end
 
   it 'ensure passing correct validations' do
     Seat.create!(cinema_number: 1, seat_number: 'a-1')
-    exception = Seat.new(cinema_number: 2, seat_number: 'a-1')
-    exception.validate
-    expect(exception.errors.count).to eql(0)
+    valid_seat = Seat.new(cinema_number: 2, seat_number: 'a-1')
+    valid_seat.validate
+    expect(valid_seat.errors.count).to eql(0)
   end
 end

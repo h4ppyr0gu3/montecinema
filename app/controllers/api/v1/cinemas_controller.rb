2dd819b191bf489c9df3a0793da7c23967757b83
David Rogers <david.rogers@monterail.com>
2021-08-11 16:09:37 +0200

    seat errors fix

--- app/controllers/api/v1/cinemas_controller.rb
+++ app/controllers/api/v1/cinemas_controller.rb
@@ -13,21 +13,23 @@ module Api
 
       def create
         cinema = Cinema.new(cinema_number: params[:cinema_number])
-        if cinema.save
-          cols = ('a'..'z').take(params[:columns]).to_a
-          rows = (1..(params[:rows])).to_a
-          seats = cols.product(rows).map(&:join)
-          seats.each do |seat_number|
-            seat = cinema.seats.new(seat_number: seat_number)
-            if seat.save
-              Rails.logger.info "cinema_number: #{params[:cinema_number]}, seat_number: #{seat_number}"
-            else
-              Rails.logger.error seat.errors.messages
+        begin
+          ActiveRecord::Base.transaction do 
+            if cinema.save!
+              cols = ('a'..'z').take(params[:columns]).to_a
+              rows = (1..(params[:rows])).to_a
+              seats = cols.product(rows).map(&:join)
+              seats.each do |seat_number|
+                seat = cinema.seats.new(seat_number: seat_number)
+                if !seat.save!
+                  Rails.logger.error seat.errors.messages
+                end
+              end
             end
           end
           render json: cinema, status: :created
-        else
-          render json: cinema.errors, status: :bad_request
+        rescue ActiveRecord::RecordInvalid => exception
+          render json: exception.messages, status: :bad_request
         end
       end
 
